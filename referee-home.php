c09acdc799bead54aac40aee6d4a0ba94d648a14
donaldCull <d_cull91@hotmail.com>
2018-09-09 17:00:20 +1000

    added refereeme homepage and included db connection result

---
+++ referee-home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>RefereeMe | Home</title>
+  </head>
+  <body>
+    <p>Database connection - <?php include 'connect.php'; ?></p>
+
+  </body>
+</html>
