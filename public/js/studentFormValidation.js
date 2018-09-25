6b602d6939d75a6cac48fbc42c7e8656c2d0a7dd
donaldCull <d_cull91@hotmail.com>
2018-09-26 07:32:38 +1000

    other selection is disabled based on drop down selection

--- public/js/studentFormValidation.js
+++ public/js/studentFormValidation.js
@@ -1,14 +1,18 @@
 function validateForm() {
 // check if either the dropdown has been used or an other selection has been made
   var studentForm = document.forms.studentForm.elements;
-  var dropDown = studentForm.selectPosition.value;
-  var otherSelection = studentForm.otherSelection.value;
+  var dropDown = studentForm.selectPosition;
+  var otherSelection = studentForm.otherSelection;
   var formValid = true;
 
-    if (dropDown === "Select Position" && otherSelection === "") {
+    if (dropDown.value === "Select Position" && otherSelection.value === "") {
         document.getElementById('select-position').focus();
         document.getElementById("errorMsg").innerHTML = "Please select a position or other position must be filled out.";
         formValid = false;
+    }
+
+    else if (dropDown !== "Select Position") {
+      otherSelection.addAttribute("disabled", "")
     }
 
     // check if form inputs are long enough to be realistically valid
@@ -22,4 +26,23 @@ function validateForm() {
     // }
     //
     return formValid;
+}
+
+// toggle other-selection to be disabled based on selection
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[name="selectPosition"]').onchange=changeEventHandler;
+},false);
+
+function changeEventHandler(event) {
+    // You can use “this” to refer to the selected element.
+    if(event.target.value !== "Select Position") {
+      console.log("you selected " +  event.target.value);
+      document.getElementById('other-selection').disabled = true;
+      document.getElementById('other-selection').style.backgroundColor = "#dddddd";
+    }
+    else if (event.target.value === "Select Position") {
+      console.log("You didnt select anything");
+      document.getElementById('other-selection').disabled = false;
+      document.getElementById('other-selection').style.backgroundColor = "#fff";
+    }
 }
