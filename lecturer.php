477d9d996503da218b38c98810c98c8ef0e8d0ce
Broderick Thomsen <broderick.thomsen@my.jcu.edu.au>
2018-09-29 09:58:10 +1000

    Add files for lecturer systems, other changes
    **Pages created:**
    -lecturer.php
    -lecturerRequestForm.php
    -request-abstained.php
    -request-answered.php
    
    **Changes/Updates:**
    
    *studentRequestForm.php*
    Moved end tag for textarea for extra-details in order to remove initial whitespace that
    text box starts with.
    
    *studentNavBar.css*
    "form > input {" changed to "form > input[type=text]{" as to only apply extra width settings
    for text inputs - was interferring with radio button inputs on lecturerRequestForm.php
    "input[type=radio] {" created
    
    *studentForm.css*
    ".info-box{" created for displaying important information
    ".flex-div {", "flex-div div {" created

---
+++ lecturer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/studentForm.css">
+    <link rel="stylesheet" href="css/studentNavBar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Confirm</title>
+  </head>
+  <body>
+    <?php include 'php/menu.php'; ?>
+
+    <div class="main">
+      <div class="header">
+        <h1 class="header-title">RefereeMe</h1>
+      </div>
+      <div class="section">
+        <h1>Home page</h1>
+        <button class="form-button" type="button" name="button">
+          <a href="lecturerRequestForm.php">Barry Badrinath's request</a>
+        </button>
+      </div>
+    </div>
+  </body>
+</html>
