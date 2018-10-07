dff934de9b67a19b2afe5e62ea90f40432e7d840
donaldCull <d_cull91@hotmail.com>
2018-10-07 13:24:33 +1000

    allow multiple business selections

--- public/js/studentFormValidation.js
+++ public/js/studentFormValidation.js
@@ -1,48 +1,66 @@
 function validateForm() {
-// check if either the dropdown has been used or an other selection has been made
+  // check if either the dropdown has been used or an other selection has been made
   var studentForm = document.forms.studentForm.elements;
   var dropDown = studentForm.selectPosition;
   var otherSelection = studentForm.otherSelection;
   var formValid = true;
+  var ul = document.getElementById("business-list");
+  var businessList = ul.getElementsByTagName("li");
 
-    if (dropDown.value === "Select Position" && otherSelection.value === "") {
-        document.getElementById('select-position').focus();
-        document.getElementById("errorMsg").innerHTML = "Please select a position or other position must be filled out.";
-        formValid = false;
-    }
+  if (businessList.length === 0) {
+    document.getElementById('company').focus();
+    document.getElementById("errorMsg").innerHTML = "Please add a business.";
+    formValid = false;
+  }
 
-    else if (dropDown !== "Select Position") {
-      otherSelection.addAttribute("disabled", "")
-    }
+  if (dropDown.value === "Select Position" && otherSelection.value === "") {
+    document.getElementById('select-position').focus();
+    document.getElementById("errorMsg").innerHTML = "Please select a position or other position must be filled out.";
+    formValid = false;
+  }
 
-    // check if form inputs are long enough to be realistically valid
-    // for (var i = 0; i < studentForm.length; i++) {
-    //   let inputSize = studentForm[i].value.length
-    //   if (studentForm[i].required && inputSize < 2) {
-    //     studentForm[i].style.backgroundColor = "#FF6666";
-    //     document.getElementById("errorMsg").innerHTML = "Form fields are too small to be valid.";
-    //     formValid = false;
-    //   }
-    // }
-    //
-    return formValid;
+  return formValid;
 }
 
 // toggle other-selection to be disabled based on selection
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('select[name="selectPosition"]').onchange=changeEventHandler;
+  document.querySelector('select[name="selectPosition"]').onchange=changeEventHandler;
 },false);
 
 function changeEventHandler(event) {
-    // You can use “this” to refer to the selected element.
-    if(event.target.value !== "Select Position") {
-      console.log("you selected " +  event.target.value);
-      document.getElementById('other-selection').disabled = true;
-      document.getElementById('other-selection').style.backgroundColor = "#dddddd";
-    }
-    else if (event.target.value === "Select Position") {
-      console.log("You didnt select anything");
-      document.getElementById('other-selection').disabled = false;
-      document.getElementById('other-selection').style.backgroundColor = "#fff";
-    }
+  // You can use “this” to refer to the selected element.
+  if(event.target.value !== "Select Position") {
+    console.log("you selected " +  event.target.value);
+    document.getElementById('other-selection').disabled = true;
+    document.getElementById('other-selection').style.backgroundColor = "#dddddd";
+  }
+  else if (event.target.value === "Select Position") {
+    console.log("You didnt select anything");
+    document.getElementById('other-selection').disabled = false;
+    document.getElementById('other-selection').style.backgroundColor = "#fff";
+  }
+}
+
+
+// Create a new business list item when clicking on the "Add" button
+function newElement() {
+  document.getElementById("errorMsg").innerHTML = " ";
+  var inputValue = document.getElementById("company").value;
+  if (inputValue === '') {
+    document.getElementById('company').focus();
+    document.getElementById("errorMsg").innerHTML = "Please add a business.";
+  } else {
+    var li = document.createElement("li");
+    li.onclick = function() {remove(this)}
+    inputValue += " \u00D7"
+    var text = document.createTextNode(inputValue);
+    li.appendChild(text);
+    document.getElementById("business-list").appendChild(li);
+  }
+  document.getElementById("company").value = "";
+}
+
+
+function remove(company) {
+  company.parentNode.removeChild(company);
 }
--- public/studentRequestForm.php
+++ public/studentRequestForm.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/studentForm.css">
-    <script src="js/studentFormValidation.js" charset="utf-8"></script>
-    <link rel="stylesheet" href="css/studentNavBar.css">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/css/studentForm.css">
+  <script src="js/studentFormValidation.js" charset="utf-8"></script>
+  <link rel="stylesheet" href="css/studentNavBar.css">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Student Referee Request</title>
-  </head>
-  <body>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Student Referee Request</title>
+</head>
+<body>
 
   <?php include 'php/menu.php'; ?>
 
@@ -18,45 +18,38 @@
       <h1 class="header-title">RefereeMe</h1>
     </div>
     <div class="section">
-      <form name="studentForm" action="form-submit.php" method="post" onSubmit="return validateForm()">
-      <h2>Referral Request Form</h2>
-      <label id="errorMsg"></label><br>
+      <form name="studentForm" action="form-submit.php" method="post" onsubmit="return validateForm()">
+        <h2>Referral Request Form</h2>
+        <label id="errorMsg"></label><br>
 
-      <label for="select-position">What job are you applying for?</label><br><br>
-      <select size="1" id="select-position" name="selectPosition">
-        <option value="Select Position">Select Position</option>
-        <option value="Programmer">Programmer</option>
-        <option value="Network Security">Network Security</option>
-        <option value="Database Management">Database Management</option>
-        <option value="App Development">App Development</option>
-        <option value="Web Design">Web Design</option>
-        <option value="Data Science">Data Science</option>
-      </select><br><br>
+        <label for="select-position">What job are you applying for?</label><br><br>
+        <select size="1" id="select-position" name="selectPosition">
+          <option value="Select Position">Select Position</option>
+          <option value="Programmer">Programmer</option>
+          <option value="Network Security">Network Security</option>
+          <option value="Database Management">Database Management</option>
+          <option value="App Development">App Development</option>
+          <option value="Web Design">Web Design</option>
+          <option value="Data Science">Data Science</option>
+        </select><br><br>
 
-      <label for="other-selection">If other, please specify</label><br>
-      <input type="text" class="formInput" id="other-selection" name="otherSelection" minlength="2"><br><br>
+        <label for="other-selection">If other, please specify</label><br>
+        <input type="text" class="formInput" id="other-selection" name="otherSelection"><br><br>
 
-      <label for="company">What company or business are you applying for?</label><br>
-      <input type="text" class="formInput" id="company" name="company" required><br><br>
+        <label for="company">What companies or businesses are you applying for?</label><br>
+        <ul id="business-list" ?></ul>
 
-      <label for="location">Where is the job located?</label><br>
-      <input type="text" class="formInput" id="location" name="location" minlength="2" required><br><br>
+        <input type="text" placeholder="Business Name" class="multiple-input" id="company" name="company" >
+        <i class="fa fa-plus" onclick="newElement()"></i><br><br>
 
-      <div class="inline-form">
-        <label for="contact">Person of Contact:</label>
-        <input type="text" class="formInput" id="contact" name="contactPerson" minlength="2" required>
-
-        <label for="contact-number">Contact number:</label>
-        <input type="text" class="formInput" id="contact-number" name="contactNumber" minlength="2" required>
-      </div><br><br>
-      <label for="extra-detail">Please any extra details as necessary:</label><br><br>
-      <textarea name="extraDetails" class="formInput" id="extra-detail" minlength="2" rows="10" cols="100"></textarea><br><br>
+        <label for="extra-detail">Please any extra details as necessary:</label><br><br>
+        <textarea name="extraDetails" class="formInput" id="extra-detail" minlength="2" rows="10" cols="100"></textarea><br><br>
 
 
-      <button type="submit" class="form-button">Finish</button>
-      <button class="form-button">Cancel</button>
+        <button type="submit" class="form-button">Finish</button>
+        <button class="form-button">Cancel</button>
       </form>
 
-  </div>
+    </div>
   </body>
-</html>
+  </html>
