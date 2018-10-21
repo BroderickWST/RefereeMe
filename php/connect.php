072848fe0a23b20c0f3621bbd826fb220728d96b
Broderick Thomsen <broderick.thomsen@my.jcu.edu.au>
2018-10-22 09:48:55 +1000

    Fix Github, reupload everything

---
+++ php/connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "izbsddgh_user";
+$password = "HelloWorld!";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=izbsddgh_refereeme", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    $e->getMessage();
+    }
+?>
